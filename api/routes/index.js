458d4ad6a4300900ff2aad530b89258e74fca0bb
unknown <luis@patricia.com.ng>
2021-04-11 17:51:16 +0100

    removes extra work

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,9 @@ import express from  'express';
 
 const api = express.Router();
 
+api.get('/', (req, res, next) => {
+    res.send('Welcome to GraphQL Url Shortner.........Please go to /graphiql to shorten url')
+})
 api.get('/:slug', RedirectUrl);
 
 export default api;
